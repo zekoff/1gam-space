988730ee7a3bb13d54d400057e9affea5e57e9fa
Michael Zekoff <zekoff@gmail.com>
2015-08-15 19:36:06 +0000

    Ship orbits around planets.

--- src/state/main.js
+++ src/state/main.js
@@ -1,4 +1,5 @@
 /* global game */
+var Ship = require('../object/ship');
 
 var state = {};
 
@@ -24,40 +25,51 @@ state.create = function() {
     nearStars.fixedToCamera = true;
 
     var i, fakePlanet;
-    for (i = 0; i < 15; i++) {
+    for (i = 0; i < 100; i++) {
         fakePlanet = game.add.sprite(game.rnd.between(0, game.world.width), game.rnd.between(0, game.world.height), 'pix');
-        fakePlanet.width = 80;
-        fakePlanet.height = 80;
+        fakePlanet.width = 30;
+        fakePlanet.height = 30;
+        fakePlanet.anchor.set(0.5);
         fakePlanet.tint = 0x00FF00;
         fakePlanet.inputEnabled = true;
         fakePlanet.events.onInputUp.add(function() {
-            // ship.x = this.x;
-            // ship.y = this.y;
-            var tween = game.add.tween(ship).to({
+            ship.leaveOrbit(this);
+            game.add.tween(game.camera).to({
+                x: this.x - 400,
+                y: this.y - 300
+            }).start();
+            var twn = game.add.tween(ship).to({
                 x: this.x,
                 y: this.y
             }).start();
+            twn.onComplete.add(function(ship) {
+                ship.enterOrbit(fakePlanet);
+            });
+            twn.start();
         }, fakePlanet);
     }
 
-    ship = game.add.sprite(0, 0, 'ship');
-    ship.inputEnabled = true;
-    ship.input.enableDrag();
+    ship = new Ship();
+    // ship = game.add.sprite(0, 0, 'ship');
+    // ship.scale.set(0.4);
+    // ship.anchor.set(0.5);
+    // ship.inputEnabled = true;
+    // ship.input.enableDrag();
 };
 
 state.update = function() {
-    var ptr = game.input.activePointer;
-    if (ptr.isDown && lastDragPoint) {
-        var xDrag = ptr.position.x - lastDragPoint.x;
-        var yDrag = ptr.position.y - lastDragPoint.y;
-        nearStars.tilePosition.x += 2 / 3 * xDrag;
-        nearStars.tilePosition.y += 2 / 3 * yDrag;
-        farStars.tilePosition.x += 1 / 3 * xDrag;
-        farStars.tilePosition.y += 1 / 3 * yDrag;
-        game.camera.x -= xDrag;
-        game.camera.y -= yDrag;
-    }
-    lastDragPoint = ptr.position.clone();
+    // var ptr = game.input.activePointer;
+    // if (ptr.isDown && lastDragPoint) {
+    //     var xDrag = ptr.position.x - lastDragPoint.x;
+    //     var yDrag = ptr.position.y - lastDragPoint.y;
+    //     nearStars.tilePosition.x += 2 / 3 * xDrag;
+    //     nearStars.tilePosition.y += 2 / 3 * yDrag;
+    //     farStars.tilePosition.x += 1 / 3 * xDrag;
+    //     farStars.tilePosition.y += 1 / 3 * yDrag;
+    //     game.camera.x -= xDrag;
+    //     game.camera.y -= yDrag;
+    // }
+    // lastDragPoint = ptr.position.clone();
 };
 
 module.exports = state;
