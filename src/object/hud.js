2b2f0c64db64f56ca13d27d205a69939fb1a3f86
Michael Zekoff <zekoff@gmail.com>
2015-08-16 15:14:05 +0000

    Break out status and planet panels in HUD.

--- src/object/hud.js
+++ src/object/hud.js
@@ -1,5 +1,9 @@
 /* global Phaser, game, space */
 var PANEL_SPEED = 500; // ms
+
+/**
+Main HUD
+*/
 var Hud = function() {
     Phaser.Group.call(this, game);
     this.inputMask = game.make.image(0, 0, 'pix');
@@ -14,17 +18,8 @@ var Hud = function() {
     this.multiButton.fixedToCamera = true;
     this.multiButton.anchor.set(0, 1);
     this.add(this.multiButton);
-    this.statusPanel = game.make.image(0, 600, 'pix');
-    this.statusPanel.height = 500;
-    this.statusPanel.width = 770;
-    this.statusPanel.fixedToCamera = true;
-    this.add(this.statusPanel);
-    this.planetPanel = game.make.image(0, 30, 'pix');
-    this.planetPanel.width = 500;
-    this.planetPanel.height = 570;
-    this.planetPanel.anchor.set(1, 0);
-    this.planetPanel.fixedToCamera = true;
-    this.add(this.planetPanel);
+    this.add(this.statusPanel = new StatusPanel());
+    this.add(this.planetPanel = new PlanetPanel());
 
     this.multiButton.inputEnabled = true;
     this.multiButton.events.onInputUp.addOnce(this.showStatusPanel, this);
@@ -48,7 +43,7 @@ Hud.prototype.hidePanel = function() {
     game.add.tween(game.camera).to({
         x: space.ship.x - 400,
         y: space.ship.y - 300
-    }, PANEL_SPEED).start();;
+    }, PANEL_SPEED).start();
     tween.onComplete.add(function() {
         this.multiButton.events.onInputUp.addOnce(this.showStatusPanel, this);
         this.inputMask.inputEnabled = false;
@@ -56,6 +51,7 @@ Hud.prototype.hidePanel = function() {
     }, this);
 };
 Hud.prototype.showPlanetPanel = function(planet) {
+    this.planetPanel.setTargetPlanet(planet);
     this.multiButton.events.onInputUp.removeAll();
     game.camera.unfollow();
     game.add.tween(game.camera).to({
@@ -70,5 +66,45 @@ Hud.prototype.showPlanetPanel = function(planet) {
         this.multiButton.events.onInputUp.addOnce(this.hidePanel, this);
     }, this);
 };
+
+/**
+Planet info panel
+*/
+var PlanetPanel = function() {
+    Phaser.Group.call(this, game);
+    var background = game.make.image(0, 30, 'pix');
+    background.width = 500;
+    background.height = 570;
+    background.anchor.set(1, 0);
+    background.fixedToCamera = true;
+    this.add(background);
+    this.travelButton = game.make.button(-300, 60, 'pix', function() {
+        print('travelling');
+    }, this);
+    this.travelButton.width = 50;
+    this.travelButton.height = 30;
+    this.travelButton.tint = 0x0000ff;
+    this.travelButton.fixedToCamera = true;
+    this.add(this.travelButton);
+};
+PlanetPanel.prototype = Object.create(Phaser.Group.prototype);
+PlanetPanel.prototype.constructor = PlanetPanel;
+PlanetPanel.prototype.setTargetPlanet = function(planet) {
+    this.targetPlanet = planet;
+};
+
+/**
+Status panel
+*/
+var StatusPanel = function() {
+    Phaser.Group.call(this, game);
+    var background = game.make.image(0, 600, 'pix');
+    background.height = 500;
+    background.width = 770;
+    background.fixedToCamera = true;
+    this.add(background);
+};
+StatusPanel.prototype = Object.create(Phaser.Group.prototype);
+StatusPanel.prototype.constructor = StatusPanel;
 
 module.exports = Hud;
